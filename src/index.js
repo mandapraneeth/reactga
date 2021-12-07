77ced8faa89089d32222197734dc83724466cd3d
mandapraneeth <manda.praneeth72@gmail.com>
2021-12-07 20:35:52 +0530

    first react ga commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactGA from "react-ga";
 import Routing from './component/Routing'
-import ReactGA from 'react-ga';
-ReactGA.initialize('G-EJ1GBKXDCK');
-ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<Routing/>, document.getElementById('root'))
